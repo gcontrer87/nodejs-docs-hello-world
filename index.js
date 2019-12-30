d433648453a0a636f1ecc7c0536cb7c5dc30bbb8
gcontrer87 <46613173+gcontrer87@users.noreply.github.com>
2019-12-29 21:54:46 -0600

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Eeepale carnal!");
+    response.end("Eeepale cuatito!");
 });
 
 const port = process.env.PORT || 1337;
